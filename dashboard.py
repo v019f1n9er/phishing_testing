717b663dc53a38d444dd33c0f593dfcf57063f0d
v019f1n9er <egorkohetkov224@gmail.com>
2026-02-05 22:37:29 +0300

    Revert "Third"

--- dashboard.py
+++
@@ -1,9 +0,0 @@
-from flask import render_template
-from security import login_required
-
-def register_dashboard_routes(app):
-
-    @app.route('/')
-    @login_required
-    def dashboard():
-        return render_template('dashboard.html')
